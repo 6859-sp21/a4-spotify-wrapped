e9522eb80248a728c353566185bf7565256f6ef9
Elizabeth Hau <elizabeth.h.hau@gmail.com>
2021-04-12 23:44:02 -0400

    make circular barplot plot info about the current song. Choosing next song doesn't properly update barplot

--- js/hit_flop.js
+++ js/hit_flop.js
@@ -1,4 +1,4 @@
-var chosenSong = 0
+var chosenSong = 0;
 
 /**
  * Given a dataset, randomly choose a song to display and return the string containing the song & artist information
@@ -7,52 +7,51 @@ var chosenSong = 0
  * @returns int Index of the chosen song
  */
 function getRandomSong(data) {
-    clearResult();
-    var randIndex = Math.floor(Math.random() * data.length)
+  console.log("in get random song");
+  clearResult();
+  var randIndex = Math.floor(Math.random() * data.length);
 
-    console.log('rand index is: ')
-    console.log(randIndex)
-    var datum = data[randIndex]
-    chosenSong = datum
-    var songTitle = datum.name
-    var artist = datum.artist
-    const songElement = document.getElementById('song-title')
-    songElement.innerHTML = `${songTitle} by ${artist}`
-    return datum
-
+  console.log("rand index is: ");
+  console.log(randIndex);
+  var datum = data[randIndex];
+  chosenSong = datum;
+  var songTitle = datum.name;
+  var artist = datum.artist;
+  const songElement = document.getElementById("song-title");
+  songElement.innerHTML = `${songTitle} by ${artist}`;
+  makeCircularBarplot(chosenSong);
+  return datum;
 }
 
 function clearResult() {
-    document.getElementById('result').innerHTML = '';
+  document.getElementById("result").innerHTML = "";
 }
 
-function getSpecificSong() {
-
-}
+function getSpecificSong() {}
 
 /**
- * 
+ *
  * @param boolean userInput Whether the user thought the song was a hit or not
  */
 function isHit(userInput) {
-    // debugger
-    const correctAns = chosenSong.target
-    // console.log(chosenSong)
-    const songName = chosenSong.name
-    const artist = chosenSong.artist
+  // debugger
+  const correctAns = chosenSong.target;
+  // console.log(chosenSong)
+  const songName = chosenSong.name;
+  const artist = chosenSong.artist;
 
-    const resultElement = document.getElementById('result')
-    if (userInput == !!correctAns) {
-        resultElement.innerHTML = `<div class="alert alert-info" role="alert">
+  const resultElement = document.getElementById("result");
+  if (userInput == !!correctAns) {
+    resultElement.innerHTML = `<div class="alert alert-info" role="alert">
         Congratualtions! You are correct! "${songName}" by ${artist} was a hit!
-      </div>`
-    } else {
-        let hitOrFlopStr = 'flop'
-        if(correctAns) {
-            hitOrFlopStr = 'hit'
-        }
-        resultElement.innerHTML = `<div class="alert alert-warning" role="alert">
-        "${songName}" was actually a ${hitOrFlopStr}
-      </div>`
+      </div>`;
+  } else {
+    let hitOrFlopStr = "flop";
+    if (correctAns) {
+      hitOrFlopStr = "hit";
     }
+    resultElement.innerHTML = `<div class="alert alert-warning" role="alert">
+        "${songName}" was actually a ${hitOrFlopStr}
+      </div>`;
+  }
 }
