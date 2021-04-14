30f7141bace74b691bbfe46f58249a0825d62106
Christina Michaels <christinamichaels@Christinas-MacBook-Pro-2.local>
2021-04-13 22:58:06 -0400

    Beginning to update HTML

--- js/hit_flop.js
+++ js/hit_flop.js
@@ -19,7 +19,6 @@ function getRandomSong(data) {
   var artist = datum.artist;
   const songElement = document.getElementById("song-title");
   songElement.innerHTML = `${songTitle} by ${artist}`;
-  // makeArtist(chosenSong, data);
   return datum;
 }
 
